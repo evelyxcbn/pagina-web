ee0bb77de3c8731f9eadb3f3253a009046ff7de6
Evelyn Lara <evelynlara806@gmail.com>
2025-10-20 16:08:41 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// 📢 Mostrar mensaje al usuario cuando hace clic en "Comprar"
+function mostrarMensaje(producto) {
+  alert(`🛒 Has agregado "${producto}" a tu carrito. ¡Gracias por apoyar a Mi Tienda Escolar!`);
+}
